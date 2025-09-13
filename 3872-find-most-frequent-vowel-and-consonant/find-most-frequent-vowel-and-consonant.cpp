8b3783107f7f8ec87ab5c03b478d3755536d9625
krupakargurije <krupakargurija@gmail.com>
2025-09-13 17:14:24 +0530

    Time: 4 ms (19.67%) | Memory: 9.9 MB (44.54%) - LeetSync

---
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxFreqSum(string s) {
+        unordered_map<char,int> mp;
+        for(int i: s){
+            mp[i]++;
+        }
+        int maxVowels = 0;
+        int maxCons = 0;
+        for(auto &i : mp){
+            int temp = i.second;
+           if(i.first == 'a' || i.first == 'e' || i.first == 'i' || i.first == 'o' || i.first == 'u'){
+                maxVowels = max(maxVowels,temp);
+            }
+            else{
+                maxCons = max(maxCons,temp);
+            }
+        }
+        return maxCons + maxVowels;
+    }
+};
