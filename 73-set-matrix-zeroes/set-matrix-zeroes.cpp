8ee98b75253a606b60859f2dc3a133c0030174b6
krupakargurije <krupakargurija@gmail.com>
2025-09-17 23:05:46 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (51.34%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<pair<int,int>> p;
+        vector<int> vis(n,0);
+
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(matrix[i][j] == 0)p.push_back({i,j});
+            }
+        }
+
+        for(auto num : p){
+            int row = num.first;
+            int col = num.second;
+
+            for(int j = 0;j<n;j++){
+                matrix[row][j] = 0;
+            }
+            for(int i = 0;i<m;i++){
+                matrix[i][col] = 0;
+            }
+        }
+    }
+};
