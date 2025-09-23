9b5137013c78fbaa888abda903d83b8dcecd9bfa
krupakargurije <krupakargurija@gmail.com>
2025-09-23 10:07:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (69.84%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int i = 0,j = 0;
+        int n = version1.size(),m = version2.size();
+        while(i < n || j < m){
+            long n1 = 0,n2 = 0;
+            while(i < n && version1[i] != '.'){
+                n1 = n1*10 + (version1[i] - '0');
+                i++;
+            }
+
+            while(j < m && version2[j] != '.'){
+                n2 = n2* 10 + (version2[j] - '0');
+                j++;
+            }
+
+            if(n1 > n2)return 1;
+            if(n1 < n2)return -1;
+
+            if(i < n && version1[i] == '.')i++;
+            if(j < m && version2[j] == '.')j++;
+
+            
+        }
+        return 0;
+    }
+};
