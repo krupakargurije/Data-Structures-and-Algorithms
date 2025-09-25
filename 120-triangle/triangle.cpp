53723f21972fae0afdd2464f421072ad0a95e7c8
krupakargurije <krupakargurija@gmail.com>
2025-09-25 12:08:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (81.37%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& nums) {
-        int n = nums.size();
-        for(int i = n-2;i >= 0;i--){
+        for(int i = nums.size()-2;i >= 0;i--){
             for(int j = 0;j<nums[i].size();j++){
-                int below = nums[i+1][j];
-                int Bright = nums[i+1][j+1];
-                nums[i][j] += min(below,Bright);
+                nums[i][j] += min(nums[i+1][j],nums[i+1][j+1]);
             }
         }
         return nums[0][0];
