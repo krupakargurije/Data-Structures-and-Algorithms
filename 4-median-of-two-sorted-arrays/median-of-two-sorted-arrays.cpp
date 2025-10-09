31dc2a64b1c96539a6585768d068db39cae0ddb5
krupakargurije <krupakargurija@gmail.com>
2025-10-09 12:15:42 +0530

    Time: 0 ms (100.00%) | Memory: 95.2 MB (63.44%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> arr;
-        int i = 0, j = 0;
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
-        for (; i < nums1.size() && j < nums2.size();) {
-            if (nums1[i] <= nums2[j]) {
-                arr.push_back(nums1[i]);
-                i++;
+        int len1 = nums1.size(), len2 = nums2.size();
+        int left = 0, right = len1;
+
+        while (left <= right) {
+            int part1 = (left + right) / 2;
+            int part2 = (len1 + len2 + 1) / 2 - part1;
+
+            int maxLeft1 = (part1 == 0) ? INT_MIN : nums1[part1 - 1];
+            int minRight1 = (part1 == len1) ? INT_MAX : nums1[part1];
+            int maxLeft2 = (part2 == 0) ? INT_MIN : nums2[part2 - 1];
+            int minRight2 = (part2 == len2) ? INT_MAX : nums2[part2];
+
+            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+                if ((len1 + len2) % 2 == 0) {
+                    return (max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2.0;
+                } else {
+                    return max(maxLeft1, maxLeft2);
+                }
+            } else if (maxLeft1 > minRight2) {
+                right = part1 - 1;
             } else {
-                arr.push_back(nums2[j]);
-                j++;
+                left = part1 + 1;
             }
         }
 
-        for (; i < nums1.size(); i++) {
-            arr.push_back(nums1[i]);
-        }
-        for (; j < nums2.size(); j++) {
-            arr.push_back(nums2[j]);
-        }
-
-        int n = arr.size();
-        if (n % 2 == 1) {
-            return arr[n / 2];
-        } else {
-            return (arr[n / 2 - 1] + arr[n / 2]) / 2.0;
-        }
+        return 0.0;        
     }
 };
