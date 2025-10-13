88a7820d3a2ec437900335778d0fa1728447a069
krupakargurije <krupakargurija@gmail.com>
2025-10-13 22:21:37 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (25.10%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -46,7 +46,6 @@ public:
                 }
             }
         }
-        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (!vis[i][j] && board[i][j] == 'O') {
