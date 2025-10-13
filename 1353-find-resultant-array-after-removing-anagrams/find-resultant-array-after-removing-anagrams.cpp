00fb85f4158a159861aa5b370acdbc0c7f2828a8
krupakargurije <krupakargurija@gmail.com>
2025-10-13 22:42:23 +0530

    Time: 1 ms (58.38%) | Memory: 16.1 MB (54.91%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
@@ -5,11 +5,11 @@ public:
         vector<string> ans;
         ans.push_back(words[0]);
     
-        for(int i = 1;i<n;i++){
+        for(int i = 1;i<n;i++){ //O(n)
             string s1 = words[i];
             string s2 = words[i-1];
-            sort(s1.begin(),s1.end());
-            sort(s2.begin(),s2.end());
+            sort(s1.begin(),s1.end()); //O(k log k)
+            sort(s2.begin(),s2.end()); //O(k log k)
             if(s1 != s2){
                 ans.push_back(words[i]);
             }
