b3eedf1b24cf383b4683e8f778d2b14d66c759c0
krupakargurije <krupakargurija@gmail.com>
2025-10-18 13:52:20 +0530

    Time: 14 ms (31.06%) | Memory: 37.2 MB (46.58%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -2,42 +2,44 @@ class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
         vector<pair<int,int>> adj[n];
-        for(auto it: roads){
+        for(auto it : roads){
             adj[it[0]].push_back({it[1], it[2]});
             adj[it[1]].push_back({it[0], it[2]});
         }
-
+        
         priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<pair<long long,int>>> pq;
         vector<long long> dis(n, LLONG_MAX);
-        vector<int> ways(n, 0);
-
+        vector<int> ways(n,0);
         int mod = 1e9 + 7;
-
+        
         dis[0] = 0;
         ways[0] = 1;
-        //{cost or weight , node}
+        //{time,node}
         pq.push({0, 0});
-
+        
         while(!pq.empty()){
             auto [dist, node] = pq.top();
             pq.pop();
-
+            
             if(dist > dis[node]) continue; // skip outdated entries
-
+            
             for(auto it : adj[node]){
                 int nNode = it.first;
                 int eWt = it.second;
-
-                if(dist + eWt < dis[nNode]){
-                    dis[nNode] = dist + eWt;
-                    pq.push({dis[nNode], nNode});
+                
+                long long newDist = dist + eWt;
+                
+                if(newDist < dis[nNode]){
+                    dis[nNode] = newDist;
+                    pq.push({newDist, nNode});
                     ways[nNode] = ways[node];
                 }
-                else if(dist + eWt == dis[nNode]){
+                else if(newDist == dis[nNode]){
                     ways[nNode] = (ways[nNode] + ways[node]) % mod;
                 }
             }
         }
-        return ways[n-1] % mod;
+        
+        return ways[n - 1] % mod;
     }
 };
