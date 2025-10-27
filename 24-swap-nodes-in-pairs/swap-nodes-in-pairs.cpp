61fa5bb3adf34903601c3c93d47e2d5276c74915
krupakargurije <krupakargurija@gmail.com>
2025-10-28 00:29:37 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (56.82%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -13,21 +13,24 @@ public:
     ListNode* swapPairs(ListNode* head) {
         if(!head || !head->next)return head;
 
-        ListNode* temp = new ListNode(0);
-        temp->next = head;
+        ListNode* temp = new ListNode(0);//create a dummy node
+        temp->next = head; // add next node adress ad head for future 
 
         ListNode* prev = temp;
 
         while(head && head->next){
+            //create a first and second nodes to swap
             ListNode* first = head;
             ListNode* second = head->next;
 
+            //swap the nodes
             first->next = second->next;
             second->next = first;
-            prev->next = second;
+            prev->next = second; // to connect the prev node to the swapped node
 
+            //move the prev node to the swaped second node 
             prev = first;
-            head = first->next;
+            head = first->next; // move the head to next first node to swap....
         }
         return temp->next;
     }
