006db9c7cd10d6af1a5c905756f57748448f8962
krupakargurije <krupakargurija@gmail.com>
2025-11-04 12:24:04 +0530

    Time: 32 ms (7.99%) | Memory: 40.1 MB (6.39%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -1,30 +1,35 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     vector<int> findXSum(vector<int>& nums, int k, int x) {
         int n = nums.size();
         vector<int> ans;
 
-        for (int i = 0; i <= n - k; i++) {
+        // Iterate over all possible subarrays of length k
+        for (int i = 0; i + k <= n; i++) {
             vector<int> sub(nums.begin() + i, nums.begin() + i + k);
+
+            // Count frequencies
             unordered_map<int, int> freq;
             for (int num : sub) {
                 freq[num]++;
             }
 
+            // Store (num, freq) pairs
             vector<pair<int, int>> items(freq.begin(), freq.end());
+
+            // Sort by frequency DESC, then by value DESC
             sort(items.begin(), items.end(), [](auto &a, auto &b) {
                 if (a.second == b.second) return a.first > b.first;
                 return a.second > b.second;
             });
 
+            // Choose top-x elements
             unordered_set<int> top_x;
-            for (int j = 0; j < items.size() && j < x; j++) {
+            for (int j = 0; j < (int)items.size() && j < x; j++) {
                 top_x.insert(items[j].first);
             }
-            
+
+            // Compute x-sum of this subarray
             int x_sum = 0;
             for (int num : sub) {
                 if (top_x.count(num)) x_sum += num;
