023f7eff842ac04be59a2a3fb3631bcfad51a802
krupakargurije <krupakargurija@gmail.com>
2025-11-06 13:25:50 +0530

    Time: 272 ms (71.29%) | Memory: 353.2 MB (79.72%) - LeetSync

--- 3863-power-grid-maintenance/power-grid-maintenance.cpp
+++ 3863-power-grid-maintenance/power-grid-maintenance.cpp
@@ -1,61 +1,55 @@
+class Disjoint{
+public:
+    vector<int>par,sz;
+    Disjoint(int n){
+        sz.resize(n,1);
+        par.resize(n);
+        for(int i=0;i<n;i++){
+            par[i]=i;
+        }
+    }
+    int findpar(int u){
+        if(par[u]==u) return u;
+        return par[u]=findpar(par[u]);
+    }
+    void UnionBySize(int x,int y){
+        int xp=findpar(x),yp=findpar(y);
+        if(xp==yp) return;
+        if(sz[xp]>sz[yp]){
+            sz[xp]+=sz[yp];
+            par[yp]=xp;
+        }
+        else{
+            sz[yp]+=sz[xp];
+            par[xp]=yp;
+        }
+    }
+};
 class Solution {
 public:
-     map<int,set<int>>mp;
-     map<int,int>grid;
-
-     void dfs(vector<vector<int>>&adj,int u,vector<bool>&visited,int component){
-         visited[u] = true;
-         mp[component].insert(u);
-         grid[u] = component;
-
-         for(auto& v:adj[u]){
-            if(!visited[v]) dfs(adj,v,visited,component);
-         }
-
-         return;
-     }
     vector<int> processQueries(int c, vector<vector<int>>& connections, vector<vector<int>>& queries) {
-        set<int>st;
-        vector<vector<int>>adj(c+1);
-
-        for(auto &it:connections){
-            int u = it[0];
-            int v = it[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        Disjoint dsu(c+1);
+        for (auto& edge : connections) {
+            dsu.UnionBySize(edge[0], edge[1]);
         }
 
-        vector<bool>visited(c+1,false);
-
-        int component = 0;
-
-        for(int i=1;i<=c;i++){
-            if(!visited[i]){
-                dfs(adj,i,visited,component);
-                component++;
-            }
+        unordered_map<int, set<int>> online;
+        for (int i=1; i<=c; ++i) {
+            online[dsu.findpar(i)].insert(i);
         }
 
-        vector<int>ans;
-
-        for(auto &q : queries){
-            int node = q[1];
-            int type = q[0];
-            int comp = grid[node];
-            if(type==2){
-                mp[comp].erase(node);
-                st.insert(node);
-            }
-            else{
-               if(!st.count(node)) ans.push_back(node);
-               else{
-                   if(!mp[comp].empty()) ans.push_back(*mp[comp].begin());
-                   else ans.push_back(-1);
-               }
+        vector<int> res;
+        for (auto& q : queries) {
+            int type=q[0], x=q[1];
+            int root=dsu.findpar(x);
+            if (type==1) {
+                if (online[root].count(x)) res.push_back(x);
+                else if (!online[root].empty()) res.push_back(*online[root].begin());
+                else res.push_back(-1);
+            } else {
+                online[root].erase(x);
             }
         }
-
-        return ans;
-
+        return res;
     }
 };
