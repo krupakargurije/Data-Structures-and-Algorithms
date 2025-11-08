246485948be3ffcb7634d72024555d3d09717bf6
krupakargurije <krupakargurija@gmail.com>
2025-11-08 10:24:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (80.63%) - LeetSync

--- 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int minimumOneBitOperations(int n) {
         int ans = 0;
-        while (n) {
-            ans ^= n;   
-            n >>= 1;    
+        while(n){
+            ans ^= n;
+            n >>= 1;
         }
-        return ans;    
+        return ans;
     }
 };
