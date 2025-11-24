c9af682a1fb9c021b2c0c0f75f2e773b89df160a
krupakargurije <krupakargurija@gmail.com>
2025-11-24 10:36:25 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (37.78%) - LeetSync

---
+++ 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        int num = 0;
+        vector<bool> ans;
+        for(int i : nums){
+            num = (num * 2 + i) % 5;
+            ans.push_back(num == 0);
+        }
+        return ans;
+    }
+};
