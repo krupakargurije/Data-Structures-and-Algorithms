5936cf2c213121a9c52ef18c5ade888d8b87aca7
krupakargurije <krupakargurija@gmail.com>
2025-12-13 22:39:57 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (67.11%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,20 +1,22 @@
 class Solution {
-    private:
-    int helper(vector<vector<int>>& arr,int row,int col,vector<vector<int>>& dp){
-        if(row < 0 || col < 0)return 1e9;
-        if(dp[row][col] != -1)return dp[row][col];
-        if(row == 0 && col == 0)return dp[0][0] = arr[0][0];
-
-        int up = helper(arr,row-1,col,dp);
-        int left = helper(arr,row,col - 1,dp);
-
-        return dp[row][col] = arr[row][col] + min(up, left);
-    }
 public:
     int minPathSum(vector<vector<int>>& arr) {
         int n = arr.size();
         int m = arr[0].size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return helper(arr,n-1,m-1,dp);
+        dp[0][0] = arr[0][0];
+
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(i == 0 && j == 0)continue;
+                else{
+                    int left = (i > 0)?dp[i-1][j]:1e9;
+                    int up = (j > 0)?dp[i][j-1]:1e9;
+
+                    dp[i][j] = arr[i][j] + min(left,up);
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
