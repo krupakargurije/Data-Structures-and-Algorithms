57fd848b65ec45cf9045c6f63248ad33bf9463a9
krupakargurije <krupakargurija@gmail.com>
2025-12-13 21:56:11 +0530

    Time: 3 ms (10.61%) | Memory: 11.8 MB (51.40%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -7,15 +7,18 @@ public:
         if (arr[0][0] == 1) return 0;
 
         vector<vector<int>> dp(n, vector<int>(m, 0));
-        dp[0][0] = 1;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (arr[i][j] == 1) {
                     dp[i][j] = 0;
-                } else {
-                    if (i > 0) dp[i][j] += dp[i - 1][j];
-                    if (j > 0) dp[i][j] += dp[i][j - 1];
+                } 
+                else if(i == 0 && j == 0)dp[i][j] = 1;
+                else {
+                    int right = (i > 0)?dp[i-1][j]:0;
+                    int down = ( j > 0)?dp[i][j-1]:0;
+
+                    dp[i][j] = right + down;
                 }
             }
         }
