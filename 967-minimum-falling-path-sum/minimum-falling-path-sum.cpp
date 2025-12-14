a79a42fda5ddc10fef89b31110a52473d594a1e3
krupakargurije <krupakargurija@gmail.com>
2025-12-14 13:45:00 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (81.45%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -4,22 +4,23 @@ public:
         int n = arr.size();
         int m = arr[0].size();
 
-        vector<vector<int>> dp(n, vector<int>(m, 0));
+        vector<int>prev(m,0),curr(m,0);
 
         // Base case: first row
         for (int j = 0; j < m; j++) {
-            dp[0][j] = arr[0][j];
+            prev[j] = arr[0][j];
         }
 
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                int up = dp[i - 1][j];
-                int left = (j > 0) ? dp[i - 1][j - 1] : 1e9;
-                int right = (j < m - 1) ? dp[i - 1][j + 1] : 1e9;
+                int up = (i > 0)?prev[j]:1e9;
+                int left = (j > 0) ? prev[j - 1] : 1e9;
+                int right = (j < m - 1) ? prev[j + 1] : 1e9;
 
-                dp[i][j] = arr[i][j] + min({up, left, right});
+                curr[j] = arr[i][j] + min({up, left, right});
             }
+            prev = curr;
         }
-        return *min_element(dp[n - 1].begin(), dp[n - 1].end());
+        return *min_element(prev.begin(), prev.end());
     }
 };
