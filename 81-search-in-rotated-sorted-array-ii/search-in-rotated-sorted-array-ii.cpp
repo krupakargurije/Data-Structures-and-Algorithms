7b681054529ca75febd592d0f838651aaa2f0095
krupakargurije <krupakargurija@gmail.com>
2025-12-29 22:05:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (99.47%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -14,11 +14,11 @@ public:
                 right--;
             }
             else if(nums[left] <= nums[mid]){
-                if(nums[left] <= target && target < nums[mid]) right = mid - 1;
+                if(nums[left] <= target && target <= nums[mid]) right = mid - 1;
                 else left = mid + 1;
             }
             else{
-                if(nums[mid] < target && target <= nums[right])left = mid + 1;
+                if(nums[mid] <= target && target <= nums[right])left = mid + 1;
                 else right = mid - 1;
             }
         }
