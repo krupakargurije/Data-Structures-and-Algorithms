a6bdd0d85e1dfde5544c43334ed0667e3157e51c
krupakargurije <krupakargurija@gmail.com>
2026-01-11 11:25:36 +0530

    Time: 8 ms (47.16%) | Memory: 21.5 MB (13.98%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -58,4 +58,4 @@ public:
         return ans;
     }
 };
-auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
+// auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
