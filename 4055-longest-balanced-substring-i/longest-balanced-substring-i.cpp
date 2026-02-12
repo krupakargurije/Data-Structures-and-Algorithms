b3f2a169a4da7171905e35c849972cea0c9010dd
krupakargurije <krupakargurija@gmail.com>
2026-02-12 17:47:13 +0530

    Time: 115 ms (79.88%) | Memory: 26.1 MB (32.93%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -1,25 +1,44 @@
-class Solution {
-public:
-    int longestBalanced(string s) {
-        int n = s.length();
-        int ans = 0;
+class Solutions{
+    public:
+    int helper(string s){
+        string str = s;
+        int n = str.size();
+        int maxlen = 0;
 
-        for(int i = 0; i < n; i++) {
-            unordered_map<char,int> mp;
-            int maxFreq = 0;
+        for(int i = 0;i<n;i++){
+            vector<int> mp(26,0);
+            int dis = 0;
+            for(int j = i;j<n;j++){
+                int idx = str[j] - 'a';
+                mp[idx]++;
 
-            for(int k = i; k < n; k++) {
-                mp[s[k]]++;
-                maxFreq = max(maxFreq, mp[s[k]]);
+                int val = 0;
+                bool bal = true;
 
-                int len = k - i + 1;
-                int unique = mp.size();
-
-                if(maxFreq * unique == len) {
-                    ans = max(ans, len);
+                for(int k = 0;k<26;k++){
+                    if(mp[k] > 0){
+                        if(val == 0){
+                            val = mp[k];
+                        }else if(mp[k] != val){
+                            bal = false;
+                            break;
+                        }
+                    }
+                }
+                if(bal){
+                    maxlen = max(maxlen,j - i + 1);
                 }
             }
         }
+        return maxlen;
+    }
+};
+
+class Solution {
+public:
+    int longestBalanced(string s) {
+        Solutions st;
+        int ans = st.helper(s);
         return ans;
     }
 };
