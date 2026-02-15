c96496f9e89d3838926e3d5bd50045b3713e4d54
krupakargurije <krupakargurija@gmail.com>
2026-02-15 15:27:57 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (7.11%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,21 +1,28 @@
 class Solution {
+    private:
+    int helper(vector<vector<int>>& nums,int r,int c,vector<vector<int>>& dp){
+        if(r < 0 || c < 0 || c >= nums[0].size())return 1e9;
+        if(r == 0)return nums[r][c];
+
+        if(dp[r][c] != 1e9)return dp[r][c];
+
+        int left = nums[r][c] + helper(nums,r-1,c-1,dp);
+        int top = nums[r][c] + helper(nums,r-1,c,dp);
+        int right = nums[r][c] + helper(nums,r-1,c+1,dp);
+
+        return dp[r][c] = min({left,top,right});
+    }
 public:
-    int minFallingPathSum(vector<vector<int>>& arr) {
-        int n = arr.size();
-        int m = arr[0].size();
+    int minFallingPathSum(vector<vector<int>>& nums) {
+        int n = nums.size();
+        int m = nums[0].size();
+        int ans = INT_MAX;
 
-        vector<int>prev(m,0),curr(m,0);
+        vector<vector<int>>dp(n,vector<int>(m,1e9));
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int up = prev[j];
-                int left = (j > 0) ? prev[j - 1] : 1e9;
-                int right = (j < m - 1) ? prev[j + 1] : 1e9;
-
-                curr[j] = arr[i][j] + min({up, left, right});
-            }
-            prev = curr;
+        for(int j = 0;j < m;j++){
+            ans = min(ans,helper(nums,n-1,j,dp));
         }
-        return *min_element(prev.begin(), prev.end());
+        return ans;
     }
 };
