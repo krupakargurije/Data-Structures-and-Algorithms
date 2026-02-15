5c38fc971201ab2cfcda51634c7331d181b2b9f6
krupakargurije <krupakargurija@gmail.com>
2026-02-15 13:54:02 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (9.08%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,16 +1,4 @@
 class Solution {
-    private:
-    int helper(vector<vector<int>>& nums,int r,int c,vector<vector<int>>& dp){
-        if(r >= nums.size() || c >= nums[r].size())return INT_MAX;
-        if(r == nums.size() - 1 && c < nums[r].size())return nums[r][c];
-
-        if(dp[r][c] != INT_MAX)return dp[r][c];
-
-        int down = nums[r][c] + helper(nums,r+1,c,dp);
-        int rightdown = nums[r][c] + helper(nums,r+1,c+1,dp);
-
-        return dp[r][c] = min(down,rightdown);
-    }
 public:
     int minimumTotal(vector<vector<int>>& nums) {
         int n = nums.size();
@@ -19,7 +7,21 @@ public:
         for(int i = 0; i < n; i++){
             dp[i] = vector<int>(nums[i].size(), INT_MAX);
         }
+        dp[0][0] = nums[0][0];
+        
+        for(int i = 1;i< n;i++){
+            for(int j = 0;j <= i;j++){
+                if(j < i)
+                    dp[i][j] = min(dp[i][j] , nums[i][j] + dp[i-1][j]);
+                if(j > 0)
+                    dp[i][j] = min(dp[i][j] , nums[i][j] + dp[i-1][j-1]);
+            }
+        }
 
-        return helper(nums,0,0,dp);
+        int ans = INT_MAX;
+        for(int i = 0;i<n;i++){
+            ans = min(ans,dp[n-1][i]);
+        }
+        return ans;
     }
 };
