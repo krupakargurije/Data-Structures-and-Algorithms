302450b4b0cb4105f337d2061b55ae590e7fd00b
krupakargurije <krupakargurija@gmail.com>
2026-02-15 15:04:50 +0530

    Time: 3 ms (30.34%) | Memory: 16.3 MB (41.43%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,22 +1,22 @@
 class Solution {
+    private:
+    int helper(vector<vector<int>>& arr,int r,int c,vector<vector<int>>& dp){
+        if(r < 0 || c < 0)return 1e9;
+        if(r == 0 && c == 0)return arr[0][0];
+
+        if(dp[r][c] != INT_MAX)return dp[r][c];
+
+        int top = arr[r][c] + helper(arr,r-1,c,dp);
+        int topleft = arr[r][c] + helper(arr,r,c-1,dp);
+
+        return dp[r][c] = min(top,topleft);
+    }
 public:
-    int minPathSum(vector<vector<int>>& arr) {
-        int n = arr.size();
-        int m = arr[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        dp[0][0] = arr[0][0];
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,INT_MAX));
 
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(i == 0 && j == 0)continue;
-                else{
-                    int left = (i > 0)?dp[i-1][j]:1e9;
-                    int up = (j > 0)?dp[i][j-1]:1e9;
-
-                    dp[i][j] = arr[i][j] + min(left,up);
-                }
-            }
-        }
-        return dp[n-1][m-1];
+        return helper(grid,n-1,m-1,dp);
     }
 };
