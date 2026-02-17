e113d3cc2bc165685c69ff57fa198b9e50d4a156
krupakargurije <krupakargurija@gmail.com>
2026-02-17 22:33:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -1,26 +1,22 @@
 class Solution {
-private:
-    bool helper(vector<int>& arr, int idx, int target, vector<vector<int>>& dp) {
-        if (target == 0) return true;
-        if (idx == 0) return arr[0] == target;
-
-        if (dp[idx][target] != -1)
-            return dp[idx][target];
-
-        bool notTake = helper(arr, idx - 1, target, dp);
-        bool take = false;
-
-        if (arr[idx] <= target) {
-            take = helper(arr, idx - 1, target - arr[idx], dp);
-        }
-
-        return dp[idx][target] = take || notTake;
+    private:
+    bool helper(vector<int>& arr,int sum,int idx,vector<vector<int>>& dp){
+        if(sum == 0)return true;
+        if(idx == 0)return (arr[0] == sum);
+        
+        if(dp[idx][sum] != -1)return dp[idx][sum];
+        
+        bool nonpic = helper(arr,sum,idx-1,dp);
+        bool pic = false;
+        if(sum >= arr[idx])
+            pic = helper(arr,sum - arr[idx],idx-1,dp);
+        
+        return dp[idx][sum] =  (pic || nonpic);
     }
-
-public:
+  public:
     bool isSubsetSum(vector<int>& arr, int sum) {
         int n = arr.size();
-        vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
-        return helper(arr, n - 1, sum, dp);
+        vector<vector<int>>dp(n, vector<int>(sum+1, -1));
+        return helper(arr,sum,n-1,dp);
     }
 };
