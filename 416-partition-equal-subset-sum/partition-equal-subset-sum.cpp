a50c3f12545531ee49b0314374ffc663a401256a
krupakargurije <krupakargurija@gmail.com>
2026-02-18 19:23:27 +0530

    Time: 158 ms (29.23%) | Memory: 17.4 MB (66.36%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,27 +1,41 @@
 class Solution {
-    private:
-    bool helper(vector<int>& nums,int target,int idx,int curr,vector<vector<int>> &dp){
-        if(curr == target)return true;
-        if(idx < 0 || curr > target)return false;
-        if(dp[idx][curr] != -1)return dp[idx][curr];
-
-        bool nonPic = helper(nums,target,idx - 1,curr,dp);
-        
-        bool pic = false;
-        if(curr + nums[idx] <= target)
-            pic = helper(nums,target,idx - 1,curr + nums[idx],dp);
-
-        return dp[idx][curr] = pic || nonPic;
-    }
 public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int totalSum = 0;
         for(int i : nums)totalSum += i;
+
         int tar = totalSum /2;
         if(totalSum % 2 != 0)return false;
-        vector<vector<int>> dp(n ,vector<int>(tar + 1 , -1));
 
-        return helper(nums,tar,n - 1,0,dp);
+        vector<vector<bool>> dp(n ,vector<bool>(tar + 1 , 0));
+
+        for(int i = 0;i< n;i++)dp[i][0] = true;
+
+        if(nums[0] <= tar)
+            dp[0][nums[0]] = true;
+
+        for(int idx = 1;idx<n;idx++){
+            for(int target = 1;target <= tar;target++){
+                bool nonPic = dp[idx - 1][target];
+
+                bool pic = false;
+                if(nums[idx] <= target)
+                    pic = dp[idx - 1][target - nums[idx]];
+                    
+                dp[idx][target] = pic || nonPic;
+            }
+        }
+        return dp[n-1][tar];
     }
 };
+
+
+
+
+
+
+
+
+
+
