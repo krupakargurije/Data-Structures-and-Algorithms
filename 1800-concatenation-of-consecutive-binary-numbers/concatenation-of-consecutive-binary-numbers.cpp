6c7f4a88299a2144ab2a6f9c68311be36790b048
krupakargurije <krupakargurija@gmail.com>
2026-02-28 22:39:15 +0530

    Time: 39 ms (61.75%) | Memory: 8 MB (69.80%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -3,15 +3,12 @@ public:
     int concatenatedBinary(int n) {
         int MOD = 1e9 + 7;
         long long ans = 0;
+        int len = 0;
 
         for(int i = 1;i<=n;i++){
-            int temp = i;
-            int len = 0;
 
-            while(temp > 0){
+            if ((i & (i - 1)) == 0)
                 len++;
-                temp >>= 1;
-            }
 
             ans = (((ans << len) % MOD) + i) % MOD;
         }
