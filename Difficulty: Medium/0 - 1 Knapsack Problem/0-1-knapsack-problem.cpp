572baa990e86a0938bad1e65121c32a0f3a813a2
krupakargurije <krupakargurija@gmail.com>
2026-03-01 00:12:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,21 +1,23 @@
 class Solution {
-    int helper(int bagWeight, vector<int> &val, vector<int> &wt , int idx , vector<vector<int>> &dp){
-        if(idx < 0)return 0;
-        
-        if(dp[idx][bagWeight] != -1)return dp[idx][bagWeight];
-        
-        int notPic = helper(bagWeight , val , wt , idx - 1 ,dp);
-        
-        int pic = 0;
-        if(wt[idx] <= bagWeight)
-            pic = val[idx] + helper(bagWeight - wt[idx], val , wt , idx - 1 , dp);
-            
-        return dp[idx][bagWeight] = max(pic , notPic);
-    }
   public:
     int knapsack(int w, vector<int> &val, vector<int> &wt) {
         int n = val.size();
-        vector<vector<int>>dp(n , vector<int>(w + 1, -1));
-        return helper(w , val , wt , n-1 , dp);
+        vector<int>prev(w + 1 , 0) , curr( w + 1, 0);
+        
+        for(int i = wt[0];i<=w;i++)prev[i] = val[0];
+        
+        for(int idx = 1;idx < n ;idx++){
+            for(int bagWeight = 0;bagWeight <= w;bagWeight++){
+                int notPic = prev[bagWeight];
+                
+                int pic = 0;
+                if(wt[idx] <= bagWeight)
+                    pic = val[idx] + prev[bagWeight - wt[idx]];
+                
+                curr[bagWeight] = max(pic , notPic);
+            }
+            prev = curr;
+        }
+        return prev[w];
     }
 };
