45da656f0c3c606f2a6586755c324d4a065c71b0
krupakargurije <krupakargurija@gmail.com>
2026-03-01 13:54:07 +0530

    Time: 36 ms (49.47%) | Memory: 22 MB (50.13%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -3,23 +3,24 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         if(amount == 0)return 0;
-        vector<vector<int>>dp(n , vector<int>(amount + 1 , 1e9));
+        vector<int>prev(amount + 1 , 1e9) , curr(amount + 1 ,1e9);
 
         for(int i = 0;i<=amount ; i++){
-            if(i % coins[0] == 0)dp[0][i] = i / coins[0];
+            if(i % coins[0] == 0)prev[i] = i / coins[0];
         }
 
         for(int idx = 1;idx < n;idx++){
             for(int target = 0;target <= amount;target++){
-                int nonPic = dp[idx - 1][target];
+                int nonPic = prev[target];
 
                 int pic = 1e9;
                 if(coins[idx] <= target)
-                    pic = 1 + dp[idx][target - coins[idx]];
+                    pic = 1 + curr[target - coins[idx]];
 
-                dp[idx][target] = min(pic , nonPic);
+                curr[target] = min(pic , nonPic);
             }
+            prev = curr;
         }
-        return (dp[n - 1][amount] == 1e9) ? -1 : dp[n - 1][amount];
+        return (prev[amount] == 1e9) ? -1 : prev[amount];
     }
 };
