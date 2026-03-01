8c5beaed57b02f9a49a0b34ac7269a3bfdf5ba9b
krupakargurije <krupakargurija@gmail.com>
2026-03-01 12:51:40 +0530

    Time: 51 ms (25.13%) | Memory: 48.8 MB (43.24%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,25 +1,4 @@
 class Solution {
-    // private:
-    // int helper(vector<int> &coins,int target,int idx ,vector<vector<int>> &dp){
-    //     if(idx < 0)return 1e9;
-    //     if(idx == 0){
-    //         if(target % coins[idx] == 0){
-    //             return target/coins[0];
-    //         }
-    //         else
-    //             return 1e9;
-    //     }
-
-    //     if(dp[idx][target] != -1)return dp[idx][target];
-
-    //     int nonPic = helper(coins , target , idx - 1 , dp);
-
-    //     int pic = 1e9;
-    //     if(coins[idx] <= target)
-    //         pic = 1 + helper(coins , target - coins[idx] , idx , dp);
-
-    //     return dp[idx][target] = min(pic , nonPic);
-    // }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
@@ -29,9 +8,6 @@ public:
         for(int i = 0;i<=amount ; i++){
             if(i % coins[0] == 0)dp[0][i] = i / coins[0];
         }
-
-        // int ans =  helper(coins , amount , n-1 ,dp);
-        // return (ans == 1e9) ? -1 : ans;
 
         for(int idx = 1;idx < n;idx++){
             for(int target = 0;target <= amount;target++){
