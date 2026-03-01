80f4993356657d71f7631648efbbcfacd8ea12ac
krupakargurije <krupakargurija@gmail.com>
2026-03-01 10:00:45 +0530

    Time: 3 ms (41.68%) | Memory: 16.6 MB (16.53%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int minPartitions(string n) {
-        int ans = 0;
-        for(char c : n)ans = max(ans , c - '0');
-        return ans;
+        return *max_element(n.begin() , n.end()) - '0';
     }
 };
