e212a29084b2bb29d5fdaa1ff5dbb193736850ad
krupakargurije <krupakargurija@gmail.com>
2026-03-05 13:14:09 +0530

    Time: 15 ms (53.49%) | Memory: 25.1 MB (32.03%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,31 +1,30 @@
 class Solution {
-private:
-    int helper(vector<int>& nums, int idx, int sum, int target,
-               vector<vector<int>>& dp, int offset) {
-
-        if(idx == nums.size()){
-            return sum == target;
-        }
-
-        if(dp[idx][sum + offset] != -1)
-            return dp[idx][sum + offset];
-
-        int add = helper(nums, idx + 1, sum + nums[idx], target, dp, offset);
-        int subtract = helper(nums, idx + 1, sum - nums[idx], target, dp, offset);
-
-        return dp[idx][sum + offset] = add + subtract;
-    }
-
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-
+        int n = nums.size();
         int sum = 0;
+
         for(int x : nums) sum += x;
 
-        int offset = sum;
-        vector<vector<int>> dp(nums.size(),
-                               vector<int>(2 * sum + 1, -1));
+        if(abs(target) > sum) return 0;
 
-        return helper(nums, 0, 0, target, dp, offset);
+        int offset = sum;
+
+        vector<vector<int>> dp(n, vector<int>(2 * sum + 1, 0));
+
+        // Base case for index 0
+        dp[0][nums[0] + offset] += 1;
+        dp[0][-nums[0] + offset] += 1;
+
+        for(int i = 1; i < n; i++) {
+            for(int s = -sum; s <= sum; s++) {
+                if(dp[i-1][s + offset] != 0){
+                    dp[i][s + nums[i] + offset] += dp[i-1][s + offset];
+                    dp[i][s - nums[i] + offset] += dp[i-1][s + offset];
+
+                }
+            }
+        }
+        return dp[n-1][target + offset];
     }
 };
