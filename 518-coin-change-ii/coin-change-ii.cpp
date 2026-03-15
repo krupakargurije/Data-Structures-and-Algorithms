882158b7286de25628d4477b274d84cb2327f20f
krupakargurije <krupakargurija@gmail.com>
2026-03-16 00:16:29 +0530

    Time: 23 ms (70.60%) | Memory: 10.8 MB (72.32%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -4,23 +4,21 @@ public:
     int change(int money, vector<int>& coins) {
         int n = coins.size();
 
-        vector<vector<ll>> dp(n , vector<ll>(money + 1 , 0));
-
-        for(int i = 0; i < n; i++)
-            dp[i][0] = 1;
+        vector<ll> curr(money + 1 , 1) , prev(money + 1 , 0);
 
         for(int idx = 0; idx < n; idx++){
             for(int amount = 1; amount <= money; amount++){
 
-                ll nonpick = (idx > 0) ? dp[idx-1][amount] : 0;
+                ll nonpick = (idx > 0) ? curr[amount] : 0;
 
                 ll pick = 0;
                 if(coins[idx] <= amount)
-                    pick = dp[idx][amount - coins[idx]];
+                    pick = curr[amount - coins[idx]];
 
-                dp[idx][amount] = pick + nonpick;
+                curr[amount] = pick + nonpick;
             }
+            prev = curr;
         }
-        return (int)dp[n-1][money];
+        return (int)prev[money];
     }
 };
