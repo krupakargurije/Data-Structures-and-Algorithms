0c82ba31481008d520aa681133f093711ec6dcdc
krupakargurije <krupakargurija@gmail.com>
2026-03-27 21:36:05 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (67.70%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -1,43 +1,21 @@
 class Solution {
-    private:
-    vector<int> leftRotate(vector<int> &arr){
-        int n = arr.size();
-
-        vector<int> ans;
-        for (int i = 1; i < n; i++) ans.push_back(arr[i]);
-        ans.push_back(arr[0]);
-
-        return ans;
-    }
-
-    vector<int> rightRotate(vector<int> &arr){
-        int n = arr.size();
-
-        vector<int> ans;
-        ans.push_back(arr[n-1]);
-        for (int i = 0; i < n - 1; i++) ans.push_back(arr[i]);
-
-        return ans;
-    }
 public:
-    bool areSimilar(vector<vector<int>>& arr, int k) {
-        int n = arr.size();
-        int m = arr[0].size();
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int m = mat.size();
+        int n = mat[0].size();
+        int s = k % n;
 
-        vector<vector<int>> ans = arr;
-        k %= m;
-
-        while(k--){
-            for(int i = 0;i<n;i++){
-                if(i % 2){
-                    ans[i] = rightRotate(ans[i]);
-                }
-                else{
-                    ans[i] = leftRotate(ans[i]);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i % 2 == 0) {
+                    // Even row: left shift by s
+                    if (mat[i][j] != mat[i][(j + s) % n]) return false;
+                } else {
+                    // Odd row: right shift by s
+                    if (mat[i][j] != mat[i][(j - s + n) % n]) return false;
                 }
             }
         }
-
-        return ans == arr;
+        return true;
     }
 };
