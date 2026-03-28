333d5e99d159ad88cf4bcc506b4a29881f843bc2
krupakargurije <krupakargurija@gmail.com>
2026-03-28 18:08:38 +0530

    Time: 0 ms (100.00%) | Memory: 112.5 MB (24.76%) - LeetSync

---
+++ 134-gas-station/gas-station.cpp
@@ -0,0 +1,23 @@
+using ll = long long;
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        ll  currGas= 0,total = 0;
+        int start = 0;
+
+        for(int i = 0;i<n;i++){
+            ll diff = gas[i] - cost[i];
+            currGas += diff;
+            total += diff;
+
+            if(currGas < 0){
+                start = i + 1;
+                currGas = 0;
+            }
+        }
+
+        if(total < 0)return -1;
+        return start;
+    }
+};
