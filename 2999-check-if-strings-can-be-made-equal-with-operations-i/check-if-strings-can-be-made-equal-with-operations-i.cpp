b772c0de844e1d171db72a512b8983026db410bc
krupakargurije <krupakargurija@gmail.com>
2026-03-29 13:20:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (49.03%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -3,8 +3,9 @@ public:
     bool canBeEqual(string s1, string s2) {
         if(s1 == s2)return true;
 
-        for(int i = 0;i<2;i++){
-            if(s1[i] != s2[i]){
+        for(int i = 0;i<4;i++){
+            if(s1[i] == s2[i])continue;
+            if(i + 2 < 4){
                 swap(s1[i] , s1[i + 2]);
             }
         }
