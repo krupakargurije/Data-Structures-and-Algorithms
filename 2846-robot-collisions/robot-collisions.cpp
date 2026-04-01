990bec810248d4f8aea74012797aae86574ede1b
krupakargurije <krupakargurija@gmail.com>
2026-04-01 22:10:25 +0530

    Time: 30 ms (99.80%) | Memory: 203.6 MB (34.93%) - LeetSync

--- 2846-robot-collisions/robot-collisions.cpp
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -3,50 +3,45 @@ public:
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
         int n = positions.size();
         
-        // {position, index}
         vector<pair<int,int>> robots;
         for(int i = 0; i < n; i++) {
             robots.push_back({positions[i], i});
         }
         
-        // sort by position
         sort(robots.begin(), robots.end());
         
-        stack<int> st; // store indices of robots moving right
+        stack<int> st;
         
-        for(auto &it : robots) {
-            int i = it.second;
-            
-            if(directions[i] == 'R') {
-                st.push(i);
-            } else {
-                // current robot is moving left
-                while(!st.empty() && healths[i] > 0) {
+        for(auto &it : robots){
+            int idx = it.second;
+
+            if(directions[idx] == 'R'){
+                st.push(idx);
+            }
+            else{
+                while(!st.empty() && healths[idx] > 0){
                     int j = st.top();
-                    
-                    if(healths[j] < healths[i]) {
-                        // right robot dies
+
+                    if(healths[j] == healths[idx]){
                         st.pop();
                         healths[j] = 0;
-                        healths[i] -= 1;
-                    }
-                    else if(healths[j] > healths[i]) {
-                        // left robot dies
-                        healths[i] = 0;
-                        healths[j] -= 1;
+                        healths[idx] = 0;
                         break;
                     }
-                    else {
-                        // both die
+                    else if(healths[j] < healths[idx]){
                         st.pop();
-                        healths[i] = 0;
                         healths[j] = 0;
+                        healths[idx] -= 1;
+                    }
+                    else{
+                        healths[idx] = 0;
+                        healths[j] -= 1;
                         break;
                     }
                 }
             }
         }
-        // collect survivors in original order
+
         vector<int> ans;
         for(int i = 0; i < n; i++) {
             if(healths[i] > 0) ans.push_back(healths[i]);
