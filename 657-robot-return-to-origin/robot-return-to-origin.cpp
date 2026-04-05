2bfa5808d4196066bf0cd4884e797610547ea897
krupakargurije <krupakargurija@gmail.com>
2026-04-05 11:44:50 +0530

    Time: 3 ms (46.40%) | Memory: 10.3 MB (76.78%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        pair<int , int> start = {0 , 0};
+        pair<int , int> curr = {0 , 0};
+
+        for(char &c : moves){
+            if(c == 'U'){
+                curr.first -= 1;
+            }
+            else if(c == 'R'){
+                curr.second += 1;
+            }
+            else if(c == 'D'){
+                curr.first += 1;
+            }
+            else{
+                curr.second -= 1;
+            }
+        }
+        return start == curr;
+    }
+};
