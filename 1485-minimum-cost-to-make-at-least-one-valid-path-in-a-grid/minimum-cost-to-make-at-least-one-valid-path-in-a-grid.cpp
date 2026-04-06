24819782224ac4306ce615204ffc0336ea40ef7f
krupakargurije <krupakargurija@gmail.com>
2026-04-07 00:50:53 +0530

    Time: 81 ms (20.92%) | Memory: 18.3 MB (82.44%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -4,19 +4,17 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
-        priority_queue<
-            pair<int, pair<int,int>>,
-            vector<pair<int, pair<int,int>>>,
-            greater<pair<int, pair<int,int>>>
-        > pq;
+        // {cost , row , col}
+        priority_queue<pair<int , pair<int , int>> ,
+            vector<pair<int , pair<int , int>>> ,
+            greater<pair<int , pair<int , int>>>
+        >pq;
 
-        vector<vector<int>> dist(n, vector<int>(m, 1e9));
-
+        vector<vector<bool>> vis(n , vector<bool> ( m, 0));
         vector<int> delRow = {0 ,0 , 0 , 1 , -1};
         vector<int> delCol = {0 ,1 , -1 , 0 , 0};
 
         pq.push({0 , {0 , 0}});
-        dist[0][0] = 0;
 
         while(!pq.empty()){
             auto it = pq.top();
@@ -26,22 +24,24 @@ public:
             int r = it.second.first;
             int c = it.second.second;
 
-            if(currCost > dist[r][c]) continue;
+            if (vis[r][c]) continue;
+            vis[r][c] = 1;
 
-            if(r == n-1 && c == m-1) return currCost;
+            if(r == n-1 && c == m-1)return currCost;
 
-            for(int i = 1; i <= 4; i++){
-                int nr = r + delRow[i];
-                int nc = c + delCol[i];
+            for(int i = 1;i <= 4;i++){
+                int nrow = r + delRow[i];
+                int ncol = c + delCol[i];
 
-                if(nr >= 0 && nr < n && nc >= 0 && nc < m){
-                    int newCost = currCost + (grid[r][c] == i ? 0 : 1);
-
-                    if(newCost < dist[nr][nc]){
-                        dist[nr][nc] = newCost;
-                        pq.push({newCost , {nr , nc}});
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m){
+                    if(grid[r][c] != i){
+                        pq.push({currCost + 1,{nrow , ncol}});
+                    }
+                    else{
+                        pq.push({currCost ,{nrow , ncol}});
                     }
                 }
+                
             }
         }
         return -1;
