6ce4cb5ddcb0517d1b24988990b606ba46b4b93b
krupakargurije <krupakargurija@gmail.com>
2026-05-02 18:22:45 +0530

    Time: 3 ms (53.12%) | Memory: 31.8 MB (15.02%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,44 +1,48 @@
 class Solution {
+    private:
+    int helper(int r , int c , vector<vector<int>>& grid , vector<vector<int>>& vis){
+        int n = grid.size();
+        int m = grid[0].size();
+
+        queue<pair<int , int>>q;
+        int delRow[] = {-1 , 0 , 1 , 0};
+        int delCol[] = {0 , 1 , 0 , -1};
+
+        q.push({r , c});
+        vis[r][c] = 1;
+
+        int area = 0;
+        while(!q.empty()){
+            auto [row , col] = q.front();
+            q.pop();
+            area++;
+
+            for(int i = 0;i<4;i++){
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m 
+                && grid[nrow][ncol] && !vis[nrow][ncol]){
+                    q.push({nrow , ncol});
+                    vis[nrow][ncol] = 1;
+                }
+            }
+        }
+        return area;
+    }
 public:
-    int maxAreaOfIsland(vector<vector<int>>& arr) {
-        int n = arr.size();
-        int m = arr[0].size();
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>>vis(n , vector<int>(m , 0));
         int ans = 0;
 
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-
-        int delr[4] = {-1, 0, 1, 0};
-        int delc[4] = {0, 1, 0, -1};
-
-        for(int i = 0;i < n;i++){
-            for(int j = 0;j < m;j++){
-                if(arr[i][j] == 1 && !vis[i][j]){
-                    int area = 0;
-                    queue<pair<int,int>> q;
-
-                    q.push({i, j});
-                    vis[i][j] = 1;
-
-                    while(!q.empty()){
-                        auto [row, col] = q.front();
-                        q.pop();
-                        area++;
-
-                        for(int d = 0; d < 4; d++){
-                            int nrow = row + delr[d];
-                            int ncol = col + delc[d];
-
-                            if(nrow >= 0 && nrow < n &&
-                               ncol >= 0 && ncol < m &&
-                               arr[nrow][ncol] == 1 &&
-                               !vis[nrow][ncol]){
-
-                                vis[nrow][ncol] = 1;
-                                q.push({nrow, ncol});
-                            }
-                        }
-                    }
-                    ans = max(ans, area);
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(!vis[i][j] && grid[i][j]){
+                    int curr = helper(i , j , grid , vis);
+                    ans = max(ans , curr);
                 }
             }
         }
