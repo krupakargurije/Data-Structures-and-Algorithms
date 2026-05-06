9826f0eea96c8e79a3fc1b2aed29f547e8d97595
krupakargurije <krupakargurija@gmail.com>
2026-05-06 13:40:45 +0530

    Time: 3 ms (45.60%) | Memory: 9.8 MB (48.27%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        if(s.size() != t.size())return false;
         
-        if(s == t)return true;
-        return false;
+        vector<int>mp1(27 , 0) , mp2(27 , 0);
+
+        for(int i = 0;i<s.length();i++){
+            mp1[s[i] - 'a']++;
+            mp2[t[i] - 'a']++;
+        }
+        for(int i = 0;i<27;i++){
+            if(mp1[i] != mp2[i])
+                return false;
+        }
+        return true;
     }
 };
