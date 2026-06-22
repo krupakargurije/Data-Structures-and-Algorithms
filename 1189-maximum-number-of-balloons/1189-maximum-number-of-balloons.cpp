4425b344e0c4ab448d78a7e6c2583dbc967a0d64
krupakargurije <krupakargurija@gmail.com>
2026-06-22 12:21:07 +0530

    Time: 0 ms (100%), Space: 8.7 MB (99.49%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int b = 0 , a = 0 , l = 0 , o = 0 , n = 0;
+
+        for(char c : text){
+            if(c == 'b')b++;
+            else if(c == 'a')a++;
+            else if(c == 'l')l++;
+            else if(c == 'o')o++;
+            else if(c == 'n')n++;
+        }
+
+        l /= 2;
+        o /= 2;
+
+        int ans = min({b , a , l , o , n});
+        return ans;
+    }
+};
