0f0a77af14daf12a7fe64942d2fc1097a37015b3
Krupakar Gurije <krupakargurija@gmail.com>
2026-07-09 15:45:21 +0530

    Time: 65 ms (24.11%), Space: 235.8 MB (29.04%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,51 +1,20 @@
 class Solution {
-    private:
-    vector<int> parent, rank;
-
-    int findParent(int node) {
-        if (parent[node] == node)
-            return node;
-        return parent[node] = findParent(parent[node]);
-    }
-
-    void unionByRank(int u, int v) {
-        int pu = findParent(u);
-        int pv = findParent(v);
-
-        if (pu == pv) return;
-
-        if (rank[pu] < rank[pv]) {
-            parent[pu] = pv;
-        }
-        else if (rank[pv] < rank[pu]) {
-            parent[pv] = pu;
-        }
-        else {
-            parent[pv] = pu;
-            rank[pu]++;
-        }
-    }
-
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        parent.resize(n);
-        rank.assign(n, 0);
-
-        for(int i = 0;i<n;i++){
-            parent[i] = i;
-        }
+        vector<int>group(n , 0);
+        int curr = 0;
 
         for(int i = 1;i<n;i++){
-            if(nums[i] - nums[i - 1] <= maxDiff)
-                unionByRank(i , i - 1);
+            if(nums[i] - nums[i - 1] > maxDiff)
+                curr++;
+            
+            group[i] = curr;
         }
 
         vector<bool>ans;
         for(auto it : queries){
-            if(parent[it[0]] ==  parent[it[1]])
-                ans.push_back(1);
-            else
-                ans.push_back(0);
+            int u = it[0] , v = it[1];
+            ans.push_back(group[u] == group[v]);
         }
         return ans;
     }
