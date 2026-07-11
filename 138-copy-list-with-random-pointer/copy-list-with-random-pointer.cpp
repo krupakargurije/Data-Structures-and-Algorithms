ead6f8cdf7ec4dadfb2649dbda02eeec0fe55a82
Krupakar Gurije <krupakargurija@gmail.com>
2026-07-11 18:00:46 +0530

    Time: 7 ms (51.80%) | Memory: 14.8 MB (96.36%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,24 +17,43 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node* , Node*>mp;
+        if (head == NULL)
+            return NULL;
+            
         Node *temp = head;
 
+        // create copy new nodes
+        // insert them in the middle of org nodes
         while(temp){
-            mp[temp] = new Node(temp->val);
-            temp = temp->next;
+            Node *newNode = new Node(temp->val);
+
+            newNode->next = temp->next;
+            temp->next = newNode;
+
+            temp = newNode->next;
         }
 
+        // fill the random adresses in new nodes
         temp = head;
         while(temp){
-            Node *newNode = mp[temp];
-            newNode->next = mp[temp->next];
-            newNode->random = mp[temp->random];
+            temp->next->random = temp->random ? temp->random->next : NULL;
+            temp = temp->next->next;
+        }
+
+        // connect new linked list
+        // Step 3: Separate the two lists
+        temp = head;
+        Node* newHead = head->next;
+
+        while (temp) {
+            Node* copy = temp->next;
+            temp->next = copy->next;
+
+            if (copy->next)
+                copy->next = copy->next->next;
+
             temp = temp->next;
         }
-        return mp[head];
+        return newHead;
     }
 };
-
-
-
