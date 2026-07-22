db12abea734bffe445b36a18ccf00cb5abca71d0
Krupakar Gurije <krupakargurija@gmail.com>
2026-07-22 18:47:31 +0530

    Time: 3 ms (73.78%) | Memory: 15.8 MB (57.32%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,19 +1,64 @@
+using ll = long long;
+
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
         int n = nums.size();
-        long long ans = 0;
 
-        for(int i = 0;i<n;i++){
-            int mn = nums[i];
-            int mx = nums[i];
+        vector<int> nextMx(n, n), nextMn(n, n);
+        vector<int> prevMx(n, -1), prevMn(n, -1);
 
-            for(int j = i;j<n;j++){
-                mn = min(mn , nums[j]);
-                mx = max(mx , nums[j]);
-                
-                ans += (long long)(mx - mn);
-            }
+        stack<int> st;
+
+        // Previous Greatest Element
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && nums[st.top()] <= nums[i])
+                st.pop();
+
+            prevMx[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+
+        while (!st.empty()) st.pop();
+
+        // Previous Minimum Element
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && nums[st.top()] >= nums[i])
+                st.pop();
+
+            prevMn[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+
+        while (!st.empty()) st.pop();
+
+        // Next Greatest Element
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] < nums[i])
+                st.pop();
+
+            nextMx[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        while (!st.empty()) st.pop();
+
+        // Next Minimum Element
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] > nums[i])
+                st.pop();
+
+            nextMn[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        ll ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            ll maxCnt = 1LL * (i - prevMx[i]) * (nextMx[i] - i);
+            ll minCnt = 1LL * (i - prevMn[i]) * (nextMn[i] - i);
+
+            ans += 1LL * nums[i] * (maxCnt - minCnt);
         }
         return ans;
     }
