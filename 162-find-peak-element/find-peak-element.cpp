1e8d4c97dc55e8b905bfebd7f7d395a99ad26e98
Krupakar Gurije <krupakargurija@gmail.com>
2026-07-31 23:37:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (41.14%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -5,14 +5,14 @@ public:
         if(n == 1)return 0;
         int left = 0 , right = n - 1;
 
-        while(left < right){
+        while(left <= right){
             int mid = left + (right - left) /2;
 
             if(mid + 1 < n && nums[mid] < nums[mid + 1]){
                 left = mid + 1;
             }
             else
-                right = mid;
+                right = mid - 1;
         }
         return left;
     }
