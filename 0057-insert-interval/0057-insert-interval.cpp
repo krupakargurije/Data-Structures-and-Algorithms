b6baf3ff24e92ba2ea66c9bea551a18f45a7136d
Krupakar Gurije <krupakargurija@gmail.com>
2026-07-31 17:55:58 +0530

    Time: 2 ms (33.38%), Space: 21.5 MB (95.57%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals,
-                               vector<int>& newInterval) {
-
-        vector<vector<int>> ans;
+    vector<vector<int>> insert(vector<vector<int>>& nums, vector<int>& ni) {
+        vector<vector<int>>ans;
         bool inserted = false;
 
-        for (auto &curr : intervals) {
+        for(auto &curr : nums){
 
-            // Insert newInterval before curr if needed
-            if (!inserted && newInterval[0] < curr[0]) {
-
-                if (ans.empty() || ans.back()[1] < newInterval[0])
-                    ans.push_back(newInterval);
+            if(!inserted && ni[0] < curr[0]){
+                if(ans.empty() || ans.back()[1] < ni[0]){
+                    ans.push_back(ni);
+                }
                 else
-                    ans.back()[1] = max(ans.back()[1], newInterval[1]);
-
+                    ans.back()[1] = max(ans.back()[1] , ni[1]);
                 inserted = true;
             }
 
-            // Process current interval
             if (ans.empty() || ans.back()[1] < curr[0])
                 ans.push_back(curr);
             else
                 ans.back()[1] = max(ans.back()[1], curr[1]);
         }
 
-        // If newInterval is the largest interval
         if (!inserted) {
-            if (ans.empty() || ans.back()[1] < newInterval[0])
-                ans.push_back(newInterval);
+            if (ans.empty() || ans.back()[1] < ni[0])
+                ans.push_back(ni);
             else
-                ans.back()[1] = max(ans.back()[1], newInterval[1]);
+                ans.back()[1] = max(ans.back()[1], ni[1]);
         }
-
         return ans;
     }
 };
