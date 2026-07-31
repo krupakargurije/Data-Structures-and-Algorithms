3a937b6aef1c69de7723ae335167ae661376bfd0
Krupakar Gurije <krupakargurija@gmail.com>
2026-07-31 13:29:51 +0530

    Time: 5 ms (79.89%), Space: 25.3 MB (94.44%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int minimumPushes(string word) {
         int n = word.length();
-        vector<int>mp(26 , 0);
+        vector<int>mp(26 , 0); //O(26) ~ O(1)
 
-        for(int i = 0;i<n;i++){
+        for(int i = 0;i<n;i++){  // O(n)
             mp[word[i] - 'a']++;
         }
-        sort(mp.begin() , mp.end() , greater<int>());
+        sort(mp.begin() , mp.end() , greater<int>()); //O(26 log 26) ~ O(1)
 
         int ans = 0;
         int count = 0;
         int x = 1;
 
-        for(int i : mp){
+        for(int i : mp){  // O(26) ~ O(1)
             if(i == 0)
                 break;
             
@@ -30,4 +30,4 @@ public:
 
 
 // xyzxyzxyzxyz -> xyz - 4
-// abcdefgh ijklmnop qrstuvwx yz  1 * 8 , 2 * 2 3 * 8 4 * 2
+// abcdefgh ijklmnop qrstuvwx yz  1 * 8 , 2 * 2, 3 * 8, 4 * 2
