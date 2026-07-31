4da4372a82705807af05eaa654924d14521f4994
Krupakar Gurije <krupakargurija@gmail.com>
2026-07-31 15:32:09 +0530

    Time: 407 ms (5.02%), Space: 17.8 MB (60.9%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int ans = INT_MIN;
+
+        for(int i = 0;i<n;i++){
+            int curr = 1;
+            for(int j = i;j<n;j++){
+                curr *= nums[j];
+                ans = max(ans , curr);
+            }
+        }
+        return ans;
+    }
+};
