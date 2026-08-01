51208a58356f8c2626e8887aa327e7a34901daae
Krupakar Gurije <krupakargurija@gmail.com>
2026-08-01 23:54:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,34 +1,41 @@
 class Solution {
-  private:
-    bool dfs(int node, vector<int>& vis, vector<int>& path, vector<vector<int>>& adj) {
+    private:
+    bool helper(int node , vector<vector<int>> &adj ,
+    vector<int> &vis , vector<int> &path){
+        
         vis[node] = 1;
         path[node] = 1;
-
-        for (auto neigh : adj[node]) {
-            if (!vis[neigh]) {
-                if (dfs(neigh, vis, path, adj)) return true;
+        
+        for(int i : adj[node]){
+            if(!vis[i]){
+                if(helper(i , adj , vis , path))
+                    return true;
             }
-            else if (path[neigh]) {
+            
+            else if(path[i])
                 return true;
-            }
         }
-
         path[node] = 0;
         return false;
     }
-
   public:
-    bool isCyclic(int V, vector<vector<int>>& edges) {
+    bool isCyclic(int n, vector<vector<int>> &edges) {
+        vector<vector<int>>adj(n);
         
-        vector<vector<int>> adj(V);
-        for (auto &e : edges) {
-            adj[e[0]].push_back(e[1]);
+        for(auto &it : edges){
+            int u = it[0];
+            int v = it[1];
+            
+            adj[u].push_back(v);
         }
-
-        vector<int> vis(V, 0), path(V, 0);
-        for (int i = 0; i < V; i++) {
-            if (!vis[i]) {
-                if (dfs(i, vis, path, adj)) return true;
+        
+        vector<int>vis(n , 0);
+        vector<int>path(n , 0);
+        
+        for(int i = 0;i<n;i++){
+            if(!vis[i]){
+                if(helper(i , adj ,vis, path))
+                    return true;
             }
         }
         return false;
