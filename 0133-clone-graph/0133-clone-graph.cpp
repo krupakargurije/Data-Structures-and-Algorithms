716a0ff2c3c2703fb75f17de82135aa9027c54f0
Krupakar Gurije <krupakargurija@gmail.com>
2026-08-02 13:54:31 +0530

    Time: 2 ms (71.38%), Space: 11.7 MB (99.86%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (node == nullptr)
+            return nullptr;
+
+        unordered_map<Node*, Node*> mp;
+        queue<Node*> q;
+
+        mp[node] = new Node(node->val);
+        q.push(node);
+
+        while (!q.empty()) {
+            Node* curr = q.front();
+            q.pop();
+
+            for (Node* nbr : curr->neighbors) {
+                if (!mp.count(nbr)) {
+                    mp[nbr] = new Node(nbr->val);
+                    q.push(nbr);
+                }
+                mp[curr]->neighbors.push_back(mp[nbr]);
+            }
+        }
+        return mp[node];
+    }
+};
