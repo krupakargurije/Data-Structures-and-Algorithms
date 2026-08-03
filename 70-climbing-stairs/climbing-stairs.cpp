c8ce1ded2d83ce33675e5d3301bc38df8b643a22
Krupakar Gurije <krupakargurija@gmail.com>
2026-08-03 13:32:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (88.62%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -10,14 +10,14 @@ class Solution {
     //     return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
     // }
 public:
-    int climbStairs(int n) {
-        vector<int>dp(n + 1, -1);
-        dp[1] = 1;
-        dp[0] = 1;
-
+    int climbStairs(int n){
+        int curr = 1 , prev = 1;
+        
         for(int i = 2;i<=n;i++){
-            dp[i] = dp[i - 1] + dp[i - 2];
+            int temp = curr + prev;
+            prev = curr;
+            curr = temp;
         }
-        return dp[n];
+        return curr;
     }
 };
