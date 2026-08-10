36ba3e7d00ca890684d9ec4c69a740762027a12d
Krupakar Gurije <krupakargurija@gmail.com>
2026-08-10 10:21:31 +0530

    Time: 27 ms (86.45%), Space: 10.9 MB (69.73%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,22 +1,31 @@
 class Solution {
     private:
     vector<int>dp;
-    bool helper(int n){
-        if(n == 0)
-            return false;
+    // bool helper(int n){
+    //     if(n == 0)
+    //         return false;
         
-        if(dp[n] != -1)
-            return dp[n];
+    //     if(dp[n] != -1)
+    //         return dp[n];
 
-        for(int i = 1;i * i <= n;i++){
-            if(!helper(n - i * i))
-                return dp[n] = true;
-        }
-        return dp[n] = false;
-    }
+    //     for(int i = 1;i * i <= n;i++){
+    //         if(!helper(n - i * i))
+    //             return dp[n] = true;
+    //     }
+    //     return dp[n] = false;
+    // }
 public:
     bool winnerSquareGame(int n) {
-        dp.assign(n + 1 , -1);
-        return helper(n);
+        dp.assign(n + 1 , 0);
+        
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j * j <= i;j++){
+                if(!dp[i - j * j]){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
     }
 };
