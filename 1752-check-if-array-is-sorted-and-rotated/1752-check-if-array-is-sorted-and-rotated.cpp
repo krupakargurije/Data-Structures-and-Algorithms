6c57adca0ddbdf36037a74d98a0eea1182ff885a
Krupakar Gurije <krupakargurija@gmail.com>
2026-08-10 11:16:09 +0530

    Time: 0 ms (100%), Space: 11.3 MB (56.1%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -5,7 +5,7 @@ public:
         int n = nums.size();
 
         for (int i = 0; i < n; i++){
-            if(nums[(i + 1) % n] < nums[i])fault++;
+            if( nums[i] > nums[(i + 1) % n])fault++;
 
             if(fault > 1)return false;
         }
