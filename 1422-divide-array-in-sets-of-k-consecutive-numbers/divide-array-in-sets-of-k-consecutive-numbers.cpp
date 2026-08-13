ce9c9173b9488e1591cb8f628d24b76aed8c8690
Krupakar Gurije <krupakargurija@gmail.com>
2026-08-13 23:37:41 +0530

    Time: 355 ms (14.96%) | Memory: 137.3 MB (61.83%) - LeetSync

--- 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.cpp
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -10,14 +10,19 @@ public:
         }
 
         for(auto &[number , count] : mp){
-            
-            if(count == 0)continue;
+
+            if(count == 0){
+                continue;
+            }
 
             for(int i = 1;i<k;i++){
                 int req = number + i;
 
                 if(mp[req] < count)return false;
                 mp[req] -= count;
+
+                if(mp[number] == 0)
+                    mp.erase(number);
             }
         }
         return true;
