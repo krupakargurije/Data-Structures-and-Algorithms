db64f4b7976fbb736ede7704c8303a0eff4da19e
Krupakar Gurije <krupakargurija@gmail.com>
2026-08-15 20:48:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (65.16%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,20 +1,19 @@
 class Solution {
+    vector<int>dp;
+    int helper(int idx , vector<int> &nums){
+        if(idx < 0)return 0;
+
+        if(dp[idx] != -1)return dp[idx];
+        
+        int rob = nums[idx] + helper(idx - 2 , nums);
+        int skip = helper(idx - 1 , nums);
+
+        return dp[idx] = max(rob , skip);
+    }
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-
-        if (n == 1) {
-            return nums[0];
-        }
-
-        int prev = nums[0];
-        int curr = max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; i++) {
-            int temp = max(curr, nums[i] + prev);
-            prev = curr;
-            curr = temp;
-        }
-        return curr;        
+        dp.assign(n + 1 , -1);
+        return helper(n - 1 , nums);
     }
 };
