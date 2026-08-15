d63ba0eafb6ae6a633c2e13fbb77bd8a8eb5e636
Krupakar Gurije <krupakargurija@gmail.com>
2026-08-15 20:54:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (65.16%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,32 @@
 class Solution {
     vector<int>dp;
-    int helper(int idx , vector<int> &nums){
-        if(idx < 0)return 0;
+    // int helper(int idx , vector<int> &nums){
+    //     if(idx < 0)return 0;
 
-        if(dp[idx] != -1)return dp[idx];
-        
-        int rob = nums[idx] + helper(idx - 2 , nums);
-        int skip = helper(idx - 1 , nums);
+    //     if(dp[idx] != -1)return dp[idx];
 
-        return dp[idx] = max(rob , skip);
-    }
+    //     int rob = nums[idx] + helper(idx - 2 , nums);
+    //     int skip = helper(idx - 1 , nums);
+
+    //     return dp[idx] = max(rob , skip);
+    // }
 public:
-    int rob(vector<int>& nums) {
+    int rob(vector<int>& nums){
         int n = nums.size();
-        dp.assign(n + 1 , -1);
-        return helper(n - 1 , nums);
+
+        if(n == 1)return nums[0];
+
+        dp.assign(n + 1 , 0);
+
+        dp[0] = nums[0];
+        dp[1] = max(nums[0] , nums[1]);
+
+        for(int i = 2;i<n;i++){
+            int rob = nums[i] + dp[i - 2];
+            int skip = dp[i - 1];
+
+            dp[i] = max(rob , skip);
+        }
+        return dp[n-1];
     }
 };
