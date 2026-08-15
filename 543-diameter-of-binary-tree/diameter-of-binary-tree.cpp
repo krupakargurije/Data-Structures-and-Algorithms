bbb8df68a6536a293c65d665b5eea77fddc163ec
Krupakar Gurije <krupakargurija@gmail.com>
2026-08-15 23:11:18 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (73.32%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -10,14 +10,14 @@
  * };
  */
 class Solution {
-    private:
     int ans = 0;
-    int helper(TreeNode* root){
+    int helper(TreeNode *root){
         if(!root)return 0;
 
         int left = helper(root->left);
         int right = helper(root->right);
-        ans =  max(ans , left + right);
+
+        ans = max(ans , left + right);
         return max(left , right) + 1;
     }
 public:
