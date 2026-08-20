368776296ec703d1562f0266546f42fd48dff2f1
Krupakar Gurije <krupakargurija@gmail.com>
2026-08-20 22:20:40 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (14.64%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -4,14 +4,22 @@ public:
         int n = nums.size();
 
         int i = n - 2;
+
         while(i >= 0 && nums[i] >= nums[i + 1])i--;
 
         if(i >= 0){
-            int j = n-1;
+            int j = n - 1;
 
-            while(j >= 0 && nums[j] <= nums[i])j--;
-            swap(nums[j] , nums[i]);
+            while(j >= 0 && nums[i] >= nums[j])j--;
+            swap(nums[i] , nums[j]);
         }
         reverse(nums.begin() + i + 1 , nums.end());
     }
 };
+
+
+// 1 2 3 6 5 4 ->  1 2 4 5 6 3
+//     p
+
+// 1 2 4 6 5 3 -> 1 2 4 3 5 6
+
