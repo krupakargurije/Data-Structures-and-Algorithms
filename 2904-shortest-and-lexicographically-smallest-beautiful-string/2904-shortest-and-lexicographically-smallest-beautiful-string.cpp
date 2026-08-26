182d8bec7b92a0ba60ff051f740a45c6e9e8597d
Krupakar Gurije <krupakargurija@gmail.com>
2026-08-26 14:00:01 +0530

    Time: 0 ms (100%), Space: 8.6 MB (95.97%) - LeetHub

---
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int n = s.length();
+
+        int start = 0 , len = INT_MAX;
+        int left = 0 , right = 0;
+
+        int numOfOnes = 0;
+        while(right < n){
+            if(s[right] == '1')numOfOnes++;
+
+            while(numOfOnes > k){
+                if(s[left] == '1')numOfOnes--;
+                left++;
+            }
+            while (numOfOnes == k && s[left] == '0') {
+                left++;
+            }
+            
+            int currLen = right - left + 1;
+            if(numOfOnes == k && currLen < len){
+                start = left;
+                len = currLen;
+            }
+            else if(numOfOnes == k && currLen == len){
+                if(s.substr(start , len) > s.substr(left , currLen)){
+                    start = left;
+                    len = currLen;
+                }
+            }
+            right++;
+        }
+        return len == INT_MAX ? "" : s.substr(start , len);
+    }
+};
