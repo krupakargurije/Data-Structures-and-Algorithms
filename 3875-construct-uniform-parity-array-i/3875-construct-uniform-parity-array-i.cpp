a2cb6c2f568a8db9bf118d4a965f01443dd7cc6a
Krupakar Gurije <krupakargurija@gmail.com>
2026-09-02 17:36:29 +0530

    Time: 0 ms (100%), Space: 30.2 MB (67.8%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,14 @@
+class Solutions{
+    public:
+    bool helper(vector<int>& nums1){
+        return true;
+    }
+};
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        Solutions s;
+        bool ans = s.helper(nums1);
+        return ans;
+    }
+};
