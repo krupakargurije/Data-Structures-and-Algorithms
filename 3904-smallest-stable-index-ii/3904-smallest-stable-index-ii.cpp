6a1a837a61318989a251b55bcafbd539a17ed4b8
Krupakar Gurije <krupakargurija@gmail.com>
2026-09-05 17:46:20 +0530

    Time: 21 ms (18.26%), Space: 210.9 MB (15.41%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,31 @@
+class Solutions{
+    public:
+    int helper(vector<int>& nums, int k){
+        int n = nums.size();
+        vector<int> mx(n), mn(n);
+
+        mx[0] = nums[0];
+        mn[n-1] = nums[n-1];
+
+        for(int i = 1; i < n; i++)
+            mx[i] = max(mx[i-1], nums[i]);
+
+        for(int i = n - 2; i >= 0; i--)
+            mn[i] = min(mn[i+1], nums[i]);
+
+        for(int i = 0; i < n; i++){
+            if(mx[i] - mn[i] <= k){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        Solutions s;
+        int ans = s.helper(nums , k);
+        return ans;
+    }
+};
